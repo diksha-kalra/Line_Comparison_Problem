a57af278303a6f7d7967fa4f875b84c192179c17
diksha-kalra <kalradiksha11@gmail.com>
2020-09-25 23:32:47 +0530

    Added: Welcome message printed

---
+++ LineComparison.java
@@ -0,0 +1,12 @@
+package com.linecomparision; 
+
+public class LineComparison {
+	
+	public static void main(String[] args) {
+		
+		//Printing welcome message
+		System.out.println("Welcome to Line Comparision Computation Program");
+
+	}
+	
+}
